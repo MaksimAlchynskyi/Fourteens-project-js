7a3f9d2f9b7bad00097eb05627dcb53e9537ab27
MaksimAlchynskyi <118046891+MaksimAlchynskyi@users.noreply.github.com>
2023-12-19 18:30:09 +0000

    Deploying to gh-pages from @ MaksimAlchynskyi/fourteens-project-js@a907c918b52b7cdff6b687d52b6b481c83020ca6 🚀

--- index.4679a61b.js
+++
@@ -1,2 +0,0 @@
-function findAndDisplayHighestNumber(){const e=findHighestNumber(parseFloat(document.getElementById("input-number-1").value),parseFloat(document.getElementById("input-number-2").value),parseFloat(document.getElementById("input-number-3").value));console.log("Most Number: ",e)}function findHighestNumber(e,t,n){return Math.max(e,t,n)}
-//# sourceMappingURL=index.4679a61b.js.map
